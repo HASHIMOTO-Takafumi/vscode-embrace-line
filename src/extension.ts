1268289ba60939db871c6db15c57f54654cb9de0
HASHIMOTO Takafumi <hashimoto-takafumi@kayac.com>
2017-11-10 15:50:45 +0900

    Embrace!

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,70 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-embrace-line" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+    const disposable = vscode.commands.registerTextEditorCommand('extension.embraceLine.embrace', (textEditor, edit) => embrace(textEditor, edit));
 
     context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
+function embrace(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) {
+    textEditor.edit((editBuilder) => {
+        const line = textEditor.selection.start.line;
+        const count = getIndentCount(textEditor, line);
+
+        editBuilder.insert(new vscode.Position(line, 0),
+            generateIndent(textEditor, count) + "{\n" +
+            generateIndent(textEditor, 1));
+        editBuilder.insert(new vscode.Position(line + 1, 0),
+            generateIndent(textEditor, count) + "}\n");
+    });
+
+}
+
+function getIndentCount(textEditor: vscode.TextEditor, line: number): number {
+    let unit: string;
+    if (textEditor.options.insertSpaces)
+    {
+        unit = "";
+        for (let i = 0; i < textEditor.options.tabSize; i++)
+            unit += " ";
+    }
+    else
+    {
+        unit = "\t";
+    }
+
+    let text = textEditor.document.lineAt(line).text;
+    let count = 0;
+    while (text.length > 9)
+    {
+        if (!text.startsWith(unit))
+            break;
+        text = text.substr(unit.length, text.length - unit.length);
+        count++;
+    }
+
+    return count;
+}
+
+function generateIndent(textEditor: vscode.TextEditor, n: number): string {
+    let indent = "";
+
+    if (textEditor.options.insertSpaces)
+    {
+        for (let i = 0; i < n * <number>textEditor.options.tabSize; i++)
+            indent += " ";
+    }
+    else
+    {
+        for (let i = 0; i < n; i++)
+            indent += "\t";
+    }
+
+
+    return indent;
+}
+
 export function deactivate() {
 }
